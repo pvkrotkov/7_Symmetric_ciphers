42c0e54a0dc873850ddca33450621d582574efcb
Гагарин Ярослав <90040588+exerius@users.noreply.github.com>
2021-10-15 17:00:03 +0300

    Update ecryption.py

--- ecryption.py
+++ ecryption.py
@@ -1,4 +1,13 @@
 def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
-
-print(encrypt(2, 'Hello!'))
+    return ''.join(map(chr, [(x + k)%65536 for x in map(ord, m)]))
+def decrypt(k, m):
+    return ''.join(map(chr, [(x - k)%65536 for x in map(ord, m)]))
+def hack(text):
+    text1 = list(text)
+    symbols = {i:text1.count(i) for i in set(text1)}
+    space = max(symbols, key=symbols.get)
+    key = ord(space)-ord(" ")
+    return(decrypt(key, text))
+phrase = input()
+encoded = encrypt(10, phrase)
+print(hack(encoded))
