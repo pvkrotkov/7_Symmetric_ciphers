77d3e8240c5be618a166b2fbecf8e6ce76b8fd5a
SeymourCray <70951761+SeymourCray@users.noreply.github.com>
2021-10-20 23:23:39 +0300

    master

--- ecryption.py
+++ ecryption.py
@@ -1,14 +1,14 @@
 
 
-max_k = 100
+max_ord = 65536
 
 
 def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
+    return ''.join(map(chr, [(x + k) % max_ord for x in map(ord, m)]))
 
 
 def decrypt(k, c):
-    return ''.join(map(chr, [x-k for x in map(ord, c)]))
+    return ''.join(map(chr, [(x - k) % max_ord for x in map(ord, c)]))
 
 
 def decrypt_(c):
@@ -19,31 +19,29 @@ def decrypt_(c):
         l = (len(findall(r'{}'.format(i), c)), i) if len(
             findall(r'{}'.format(i), c)) > l[0] else l
     k = ord(l[1]) - ord(' ')
-    return ''.join(map(chr, [x-k for x in map(ord, c)]))
+    return ''.join(map(chr, [(x - k) % max_ord for x in map(ord, c)]))
 
 
-def random_int(l, maximum):
-    from random import randint
-    return [randint(1, maximum) for i in range(l)]
-
-
-def vernam(m):
-    a = random_int(len(m), max_k)
-    return ''.join(map(chr, [x + a[i] for i, x in enumerate(map(ord, m))])), a
+def vernam(k, m):
+    k = k*(len(m)//len(k)) + k[-(len(m) % len(k)):]
+    return ''.join(map(chr, [i ^ x for i, x in zip(map(ord, m), map(ord, k))]))
 
 
 def vernam_decrypt(k, c):
-    return ''.join(map(chr, [x - k[i] for i, x in enumerate(map(ord, c))]))
+    return vernam(k, c)
 
 
 k = 1000
 text = 'Место тяги было продившим от снега. Сам он вернулся на другой край, к двойняшке-березе, и, прислонив ружье к развилине сухого нижнего сучка, снял кафтан, перепоясался и попробовал свободы движений рук.Старая седая Ласка, ходившая за ним следом, села осторожно против него и насторожила уши. Солнце спускалось за крупный лес, и на свете зари березки, рассыпанные по осиннику, отчетливо рисовались своими висящими ветвями с надутыми, готовыми лопнуть почками.'
 a = encrypt(k, text)
-# print(a)
+print()
+print('Текст, зашифрованный шифром Цезаря: ', a, '\n')
 b = decrypt(k, a)
-# print(b)
+print('Текст, расшифрованный с ключом: ', b, '\n')
 c = decrypt_(a)
-# print(c)
-d, key = vernam(text)
-# print(d)
-#print(vernam_decrypt(key, d))
+print('Без ключа: ', c, '\n')
+k = input('ключ для Вернама: ')
+print()
+d = vernam(k, text)
+print('Шифротекст Вернама: ', d, '\n')
+print('Расшифрованный текст: ', vernam_decrypt(k, d))
