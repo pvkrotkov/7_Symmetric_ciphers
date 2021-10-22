0ed96fcc3ef929aa1972e1750f6caf6d5e455cef
Гагарин Ярослав <gagarinyaroslav3@gmail.com>
2021-10-22 17:09:01 +0300

    Исправлено

--- encryption.py
+++ encryption.py
@@ -1,17 +1,23 @@
-def encrypt(k, m):
+def encrypt(k, m): # шифрование текста
     return ''.join(map(chr, [(x + k)%65536 for x in map(ord, m)]))
 
 
-def decrypt(k, m):
+def decrypt(k, m): # расшифрование текста
     return ''.join(map(chr, [(x - k)%65536 for x in map(ord, m)]))
 
 
-def hack(text):
+def hack(text): # взлом текста (расшифрование без ключа) методом частотного анализа
     text1 = list(text)
     symbols = {i:text1.count(i) for i in set(text1)}
-    space = max(symbols, key=symbols.get)
-    key = ord(space)-ord(" ")
-    return(decrypt(key, text))
-phrase = input()
-encoded = encrypt(10, phrase)
-print(hack(encoded))
+    space = max(symbols, key=symbols.get) # ищем символ соотв. пробелу
+    key = ord(space)-ord(" ") # находим число-ключ
+    return(decrypt(key, text))  #расшифровываем
+
+
+phrase = input("Введите текст для шифрования")
+seed = input("Введите число")
+encoded = encrypt(seed, phrase)
+print("Закодированый текст")
+print(encoded)
+print("Раскодированый закодированый текст")
+print(decrypt(seed, encoded))
--- vernam.py
+++ vernam.py
@@ -1,28 +1,30 @@
 import random
 
 
-def create_key(leng):
+def create_key(leng): #генерируем ключ
     key = []
     for i in range(leng):
         key.append(random.randint(0,255))
     return key
 
 
-def encrypt(text, key):
+def encrypt(text, key): # шифруем текст
     text = list(text)
     for i in range(len(text)):
         text[i] = chr(ord(text[i]) ^ key[i])
     return text
 
 
-def decrypt(text, key):
+def decrypt(text, key): #расшифровываем текст
     return "".join(encrypt(text, key))
 
 
-text = input()
-key = create_key(len(list(text)))
-print(list(text))
-a = encrypt(text, key)
-b = decrypt(a, key)
+text = input("Введите текст")
+key = create_key(len(list(text))) # создаем ключ
+a = encrypt(text, key) # шифрование
+b = decrypt(a, key) #расшифрование
+print("Закодированый текст")
+print("".join(a))
+print("Раскодированый закодированый текст")
 print(b)
 
