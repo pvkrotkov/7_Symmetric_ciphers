a34a1e63e43e64a9b98822cf051c92df5601f073
AnnFA <90024487+AnnFA@users.noreply.github.com>
2021-11-02 12:30:37 +0300

    Алексеева А. ПИ20-1 Шифрование

---
+++ Vigenère.py
@@ -0,0 +1,20 @@
+msg = "A design is"
+key = "354укеоьдсq"
+
+
+def encrypt(msg, key):
+	a, b = [i for i in (msg, key)]
+	msg1 = "".join(map(chr, [ord(x) ^ ord(y) for x, y in [(a[i], b[i]) for i in range(len(a))]]))
+	return msg1
+
+
+def decrypt(msg1, key):
+	a, b = [i for i in (msg1, key)]
+	msg = "".join(map(chr, [ord(x) ^ ord(y) for x, y in [(a[i], b[i]) for i in range(len(a))]]))
+	return msg
+
+
+e = encrypt(msg, key)
+d = decrypt(e, key)
+print(e)
+print(d)
