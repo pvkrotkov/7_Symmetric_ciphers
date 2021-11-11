bbc49a1a4fe504c0ca5d38df8ed4bca7ea7acc0a
Daniel <90207052+Daniel2727272727@users.noreply.github.com>
2021-11-11 16:43:43 +0300

    Update ecryption.py

--- ecryption.py
+++ ecryption.py
@@ -1,4 +1,47 @@
-def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
+def decrypt(word):
+    keys = {}
+    for letter in word:
+        keys[letter] = keys.get(letter, 0) + 1
+    key = sorted(keys.items(), key=lambda x: x[1], reverse=True)[0]
+    return chr(ord(key[0]) - ord(" "))
 
-print(encrypt(2, 'Hello!'))
+
+def encryption(word, key, decode=False):
+    ls = []
+    for i in word:
+        if decode:
+            ls.append(chr((ord(i) - ord(key)) % 65536))
+        else:
+            ls.append(chr(ord(i) + ord(key) % 65536))
+    return "".join(ls)
+
+def decode_encode(text, key): #XOR
+    ls = []
+    for a in range(len(text)):
+        for i, j in [(text[a], key[a])]:
+            ls.append(ord(i) ^ ord(j))
+    return "".join(map(chr, ls))
+
+
+text = "Hello world"
+key = "jgqwertagsh"
+enc = decode_encode(text, key)
+
+print(f'Запуск...')
+dec = decode_encode(enc, key)
+
+print()
+print(f'Шифрование...')
+print()
+if __name__ == '__main__':
+
+    text = "My name is Daniel"
+    key = "I"
+
+    encrypted_text = encryption(text, key)
+    decrypted_text = encryption(encrypted_text, key, decode=True)
+
+    print(f"{text} -> «{encrypted_text}» -> {decrypted_text}")
+
+    print(f"Подобранный ключ: {decrypt(encrypted_text)}")
+    print()
