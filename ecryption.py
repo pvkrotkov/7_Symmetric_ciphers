f69ab405bc96a598e55d81fef5cf76d405768e63
Никита Орайло <90414850+6Nkita9@users.noreply.github.com>
2021-11-17 22:04:19 +0300

    Update ecryption.py

--- ecryption.py
+++ ecryption.py
@@ -1,82 +1,51 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 17 13:58:00 2019
-@author: polin
-"""
-import random
-
-def xor(text,key):
-    """ XOR двух текстов (одинаковой длины) """
-    result=str()
-    for i in range(len(key)):
-        text_ord=ord(text[i]) 
-        key_ord=ord(key[i])
-        result+=chr(text_ord^key_ord)
-    return result
-       
-def segment(message, key_length):
-    """ Сегментирование текста по размеру ключа. 
-    Возвращает список сегментов либо сегмент под определённым номером """
-    num_of_segments=int(len(message)/key_length)
-    result=[message[i*key_length:(i+1)*key_length]for i in range(num_of_segments)]
-    return result
-
-def add_char(message,key_length,char=" "):
-    """ Дополнение сообщение символами для кратности длине ключа """
-    if (len(message)%key_length!=0):
-        message+=char*(key_length-(len(message)%key_length))
-    return message
-
-def randomize(length):
-    """ Генерация строки случайных символов """
-    result=str()
-    for i in range(length): #при больших рандомных числах программа ломается с ошибкой
-        result+=chr(random.randint(0,10000)) # surrogates not allowed 
-    return result
-
-def encrypt(message,key,iv):
-    """ Шифрование текста """
-    result=list()
-    message=add_char(message,len(key)) # добиваем сообщение и IV до кратности
-    iv=add_char(iv,len(key))           # длине ключа
-    segmented_message=segment(message,len(key)) # сегментируем сообщение
-    num_of_segments=len(segmented_message)
-    
-    result.append(iv) # добавляем IV в шифроткст
-    result.append(xor(segmented_message[0],iv)) 
-    
-    for i in range(num_of_segments-1): 
-        result.append(xor(xor(result[i+1],segmented_message[i+1]),key))
-        
-    return "".join(result)
-
-def decrypt(message,key):
-    """ Дешифрование текста"""
-    result=list()
-    segmented_message=segment(message,len(key))
-    iv=segmented_message[0]
-    del segmented_message[0] #удаляем IV из сегментированного сообщения
-    num_of_segments=len(segmented_message)   
-    
-    for i in range(num_of_segments-1,-1,-1): #дешифруем в обратном порядке
-        result.append(xor(xor(segmented_message[i],key),segmented_message[i-1]))
-    
-    result[-1]=xor(segmented_message[0],iv)
-    result=result[::-1]
-    
-    return "".join(result)
-
-message="Привет, как дела?"
-key=randomize(4)
-iv=randomize(4)
-
-print("Сообщение {}".format(message))
-print("Ключ {}".format(key))
-print("IV {}".format(iv))
-
-encrypted=encrypt(message,key,iv)
-print("Зашифрованное сообщение {}".format(encrypted))
+from collections import Counter
+from random import randint
+N = 65536
 
 
-decrypted=decrypt(encrypted,key)
-print("Дешифрованное сообщение {}".format(decrypted))
+def encrypt_caesar(k, m):
+    offset_ords = [(x + k)%N for x in map(ord, m)]
+    return ''.join(map(chr, offset_ords))
+
+
+def decrypt_caesar(k, m):
+    offset_ords = [(x - k)%N for x in map(ord, m)]
+    return ''.join(map(chr, offset_ords))
+
+
+def hack(m):
+    most_common = Counter(m).most_common()[0][0]
+    key = ord(most_common) - ord(' ')
+    return decrypt_caesar(key, m)
+
+
+# шифр вернама
+def generate_key(length):
+    return [randint(0,26) for _ in range(length)]
+
+def xor(char, key):
+    return chr(ord(char)^key)
+
+def encrypt_v(text):
+    key = generate_key(len(text))
+    return ''.join([xor(text[i], key[i]) for i in range(len(text))]), key
+
+def decrypt_v(cryptedtext, key):
+    return ''.join([xor(cryptedtext[i], key[i]) for i in range(len(cryptedtext))])
+
+TEXT_TO_CHECK = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
+key = int(input('Введите ключ для Цезаря: '))
+text = encrypt_caesar(key, TEXT_TO_CHECK)
+print('Зашифрованный текст Цезаря:', text)
+print('---------------------------------------------------------------------------')
+print('Расшифрованный текст Цезаря:', decrypt_caesar(key, text))
+print('-----------------------------------------------------------------------------')
+print('Взломанный текст Цезаря:', hack(text))
+print('------------------------------------------------------------------------------')
+encrypt_v, key = encrypt_v(TEXT_TO_CHECK)
+print('Зашифрованный шифром Вернама текст: ', encrypt_v)
+print('------------------------------------------------------------------------------')
+decrypt_v = decrypt_v(encrypt_v, key)
+print('Расшифрованный текст: ', decrypt_v)
+print('------------------------------------------------------------------------------')
