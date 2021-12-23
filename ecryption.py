d2cb2d656453c3a7f03c31a85d5c043306b70413
Иван Башмаченков <70896241+DudeBash@users.noreply.github.com>
2021-12-23 17:19:51 +0300

    Update ecryption.py

--- ecryption.py
+++ ecryption.py
@@ -1,4 +1,61 @@
-def encrypt(k, m):
-    return ''.join(map(chr, [x + k for x in map(ord, m)]))
+from random import randint
 
-print(encrypt(2, 'Hello!'))
+
+alfavit =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+
+
+
+def ave_cesar(message):
+    smeshenie = int(input('Шаг шифровки: '))    #Создаем переменную с шагом шифровки
+    mess = message.upper()    #создаем переменнную, куда запишем наше сообщение
+    itog = ''    #создаем переменную для вывода итогового сообщения
+    for i in mess:
+        mesto = alfavit.find(i)    #Вычисляем места символов в списке
+        new_mesto = mesto + smeshenie    #Сдвигаем символы на указанный в переменной smeshenie шаг
+    for i in mess:
+        mesto = alfavit.find(i)
+        new_mesto = mesto + smeshenie
+        if i in alfavit:
+            itog += alfavit[new_mesto]  # Задаем значения в итог
+        else:
+            itog += i
+    print (itog)
+
+# ave_cesar('Hello')
+
+
+def ne_ave_cesar(message):
+    shift = int(input('Шаг шифровки: '))
+    encrypted_text = message
+    plain_text = ""
+    for c in encrypted_text:
+        # проверить, является ли символ заглавной буквой
+        if c.isupper():
+            c_index = ord(c) - ord("A")
+            # выполнить отрицательный сдвиг
+            new_index = (c_index - shift) % 26
+            # преобразовать в новый символ
+            new_unicode = new_index + ord("A")
+            new_character = chr(new_unicode)
+            # добавление к простой строке
+            plain_text = plain_text + new_character
+        else:
+            # так как символ не является строчным, оставьте его как есть
+            plain_text += c
+    print("Расшифровано",plain_text)
+
+# ne_ave_cesar('MJQQT')
+
+
+def ave_vernam(message):
+    key=''
+    keys=''
+    final=''
+    mes = message
+    for symbol in mes:
+        key = randint(0,32); keys += str(key) + "/"
+        final += chr((ord(symbol) + key - 17)%33 + ord('А'))
+    print('Зашифрованное сообщение: ', final)
+    print ('Ключ шифрования: ',keys)
+
+# ave_vernam('Hello')
